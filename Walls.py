0487de2f1aeeaee074d5c1f639659a0dcb891b54
CraigPredatsch <craig.predatsch@gmail.com>
2016-06-27 21:47:07 -0400

    Update Walls.py

--- Walls.py
+++ Walls.py
@@ -6,7 +6,7 @@ vertical = 0                #starting point North/South
 horizontal = 0              #starting point #East/West
 
 def run():                  #Initial function asking the user what they intend to do.
-    print('Do you wish to move your character? You are currently located at location : [%d, %d]' %(vertical, horizontal))           #Asks if the user wishes to move and prints out their current location
+    print('Do you wish to move your character? You are currently located at location : [%d, %d]' %(horizontal, vertical))           #Asks if the user wishes to move and prints out their current location
     print('Press 1 if yes. Press 2 if no.')                                     #Asks the user to type '1' or '2' based on their choice.
     decision = input()                                                          #User Input
     try:
@@ -44,34 +44,34 @@ def movement():                                             #Function that will 
         if direction_check == 1:                            #User input = 1
             if vertical < 1:                                #Setting a limit for how far a user can move North.
                 vertical += 1                               #Up increment the vertical value, if the user is not already at the limit established
-                print('You have moved North. You are now at location : [%d, %d]' %(vertical, horizontal))   #Tells the user their new location
+                print('You have moved North. You are now at location : [%d, %d]' %(horizontal, vertical))   #Tells the user their new location
                 movement()                                  #Re-run movement function
             else:                                           #If user is at the Northern limit
-                print('You cannot go any further North. You are already located at location : [%d, %d]' %(vertical, horizontal))    #Tells user they can't go any further North and reminds them of their location
+                print('You cannot go any further North. You are already located at location : [%d, %d]' %(horizontal, vertical))    #Tells user they can't go any further North and reminds them of their location
                 movement()                                  #Re-run movement function
         elif direction_check == 2:                          #Repeat for user input = 2 (East)
             if horizontal < 1:
                 horizontal += 1
-                print('You have moved East. You are now at location : [%d, %d]' %(vertical, horizontal))
+                print('You have moved East. You are now at location : [%d, %d]' %(horizontal, vertical))
                 movement()
             else:
-                print('You cannot go any further East. You are already located at location : [%d, %d]' %(vertical, horizontal))
+                print('You cannot go any further East. You are already located at location : [%d, %d]' %(horizontal, vertical))
                 movement()
         elif direction_check == 3:                          #Repeat for user input = 3 (South)
             if vertical > -1:
                 vertical -= 1
-                print('You have moved South. You are now at location : [%d, %d]' %(vertical, horizontal))
+                print('You have moved South. You are now at location : [%d, %d]' %(horizontal, vertical))
                 movement()
             else:
-                print('You cannot go any further South. You are already located at location : [%d, %d]' %(vertical, horizontal))
+                print('You cannot go any further South. You are already located at location : [%d, %d]' %(horizontal, vertical))
                 movement()
         elif direction_check == 4:                          #Repeat for user input = 4 (West)
             if horizontal > -1:
                 horizontal -= 1
-                print('You have moved West. You are now at location : [%d, %d]' %(vertical, horizontal))
+                print('You have moved West. You are now at location : [%d, %d]' %(horizontal, vertical))
                 movement()
             else:
-                print('You cannot go any further West. You are already located at location : [%d, %d]' %(vertical, horizontal))
+                print('You cannot go any further West. You are already located at location : [%d, %d]' %(horizontal, vertical))
                 movement()
         elif direction_check == 5:                              #User input = 5
             print('You have stopped moving. Ending Program.')   #Tells user they have chosen to stop moving and end program
